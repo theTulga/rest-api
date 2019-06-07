826427addebd7f6d743c0e37d77b0e27c05540ba
tulga <ts.battulga18@gmail.com>
2019-06-07 15:05:13 -0700

    simple rest api done

--- main.go
+++ main.go
@@ -2,8 +2,113 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
+	// "log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
 )
 
+// Book Struct (Model)
+
+type Book struct {
+	ID  		string `json:"id"`
+	Isbn 		string `json:"isbn"`
+	Title 	string `json: "title"`
+	Author *Author `json: "author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname 	string `json:"lastname"`
+}
+
+var books []Book
+
+func getBooks (w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func createBook (w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000))  // Mock ID - not safe
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+
+func getBook (w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	// Loop through books and find with id
+	for _, item := range books {
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+func updateBook (w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range books {
+		if item.ID == params["id"]{
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+func deleteBook (w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range books {
+		if item.ID == params["id"]{
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	r := mux.NewRouter()
+	// Mock Data
+	// @todo - implement DB
+	books = append( books, 
+		Book {
+			ID: "1", 
+			Isbn: "4324234", 
+			Title: "Book One", 
+			Author: &Author{Firstname: "John", Lastname: "Legend"}})
+	books = append( books, 
+		Book {
+			ID: "2", 
+			Isbn: "53254234", 
+			Title: "Book Two", 
+			Author: &Author{Firstname: "Ceasar",Lastname: "Markus"}})
+
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	fmt.Println("Starting server on post 8080")
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		fmt.Println("Error occured on ListenAndServe %v", err)
+	}
 }
